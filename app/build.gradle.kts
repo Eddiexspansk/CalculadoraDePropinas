a29afe7e80c29d8341cd93dc869507f963a43684
Eddiexspansk <spansk_cl@hotmail.com>
2024-07-08 14:12:09 +0100

    Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,14 +6,14 @@ plugins {
 
 android {
     namespace = "com.example.calculadoradepropinas"
-    compileSdk = 33
+    compileSdk = 34
     viewBinding {
         enable = true
 
     defaultConfig {
         applicationId = "com.example.calculadoradepropinas"
-        minSdk = 21
-        targetSdk = 33
+        minSdk = 23
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -48,7 +48,7 @@ dependencies {
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation ("androidx.recyclerview:recyclerview:1.3.1")
-    implementation("com.google.android.gms:play-services-ads:22.6.0")
+    implementation("com.google.android.gms:play-services-ads:23.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -60,5 +60,4 @@ dependencies {
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation ("com.google.android.gms:play-services-ads:22.6.0")
-
 }
